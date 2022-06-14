9a59859b3913cd69bc17e360886877dc0e49f029
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-14 18:08:31 +0000

    Setup user input and ctrl c handling

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
 const {connect} = require('./client');
 
+//setup interface to handle user input from stdin
+
+const handleUserInput = function (key){
+  if (key === '\u0003'){
+    process.exit();
+  }
+}
+
+const setupInput = function (){
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+}
+
+setupInput();
 console.log("Connecting ...");
 connect();
