119c41c947714ce6f6e754baf0319f67bfbe33b3
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-14 18:14:02 +0000

    refactor play, move functions to input file

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
-//setup interface to handle user input from stdin
 
-const handleUserInput = function (key){
-  if (key === '\u0003'){
-    process.exit();
-  }
-}
-
-const setupInput = function (){
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-}
-
-setupInput();
 console.log("Connecting ...");
 connect();
+
+setupInput();
